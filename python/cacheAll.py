eeba86157753b5cabacb62dc7dafc08ba500bb7d
Kevin Pedro <kpedro88@gmail.com>
2017-09-26 14:20:55 -0500

    add cacheAll script

---
+++ python/cacheAll.py
@@ -0,0 +1,31 @@
+import os
+from parseConfig import parser_dict
+
+def cacheOne(dir,filepath):
+    if os.path.isfile(filepath):
+        print "Already cached "+dir
+    else:
+        print "Cache "+dir
+        with open(filepath,'w') as cachefile:
+            cachefile.write('Signature: 8a477f597d28d172789f06886806bc55\n')
+            cachefile.write('# This file is a cache directory tag.\n')
+            cachefile.write('# For information about cache directory tags, see:\n')
+            cachefile.write('#       http://www.brynosaurus.com/cachedir/')
+
+def uncacheOne(dir,filepath):
+    if os.path.isfile(filepath):
+        print "Uncache "+dir
+        os.remove(filepath)
+    else:
+        print "Already uncached "+dir
+
+def cacheAll():
+    for dir,val in parser_dict['caches'].iteritems():
+        filepath = os.path.join(os.path.expandvars(dir),'CACHEDIR.TAG')
+        if val=='1':
+            cacheOne(dir,filepath)
+        elif val=='0':
+            uncacheOne(dir,filepath)
+
+if __name__=="__main__":
+    cacheAll()
